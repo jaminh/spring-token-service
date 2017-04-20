07faa0669bfb9338529b73a6e86821df92e06e36
Jamin Hitchcock <jamin.hitchcock@gmail.com>
2017-04-19 20:15:48 -0500

    Initial commit

---
+++ src/main/java/com/jaminh/ws/SpringTokenServiceApplication.java
@@ -0,0 +1,12 @@
+package com.jaminh.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTokenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTokenServiceApplication.class, args);
+	}
+}
